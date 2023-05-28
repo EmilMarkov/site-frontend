8af5b04d2ff55b53b79062488b080310016da254
Markov Emil <69921054+EmilMarkov@users.noreply.github.com>
2023-05-28 13:21:14 +0300

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -303,7 +303,7 @@ function saveData() {
 	updateCookie('email', result.email)
 	updateCookie('password', result.password)
 
-	fetch('https://nevidomiy-production.up.railway.app/update', {
+	fetch('https://site-backend-production.up.railway.app/update', {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
